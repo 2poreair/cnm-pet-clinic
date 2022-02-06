652e5d15cb6e77a5bacfd9e909ce20b9b805e036
2pore <jgaylor84@gmail.com>
2022-02-05 18:51:47 -0600

    adding base entity. Closes #10

--- snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/model/Person.java
+++ snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package cnm.snax.snaxpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     
     private String firstName;
     private String lastName;
--- snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/model/PetType.java
+++ snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package cnm.snax.snaxpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
