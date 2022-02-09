0fb4f8adaf0de428d7008b1414bb7ead689f8d83
2pore <jgaylor84@gmail.com>
2022-02-08 20:52:45 -0600

    refactoring to commmon crud interface.  Closes #11

--- snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/services/OwnerService.java
+++ snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package cnm.snax.snaxpetclinic.services;
 
 import cnm.snax.snaxpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLasName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/services/VetService.java
+++ snax-pet-clinic/pet-clinic-data/src/main/java/cnm/snax/snaxpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package cnm.snax.snaxpetclinic.services;
 
 import cnm.snax.snaxpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
